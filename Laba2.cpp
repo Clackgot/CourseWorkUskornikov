4ba7f705a95c6e8f2016f8325449ded00af52f41
clackgot <clackgot@gmail.com>
2020-05-16 09:09:07 +0300

    remove comment

--- Laba2.cpp
+++ Laba2.cpp
@@ -34,11 +34,6 @@ string getText(string msg)
     cout << msg;
     while (true)
     {
-        /*
-        m:
-        getline(cin, temp);
-        if (temp == "")goto m;
-        */
         while(temp == "")getline(cin, temp);
 
         if (temp == "")
